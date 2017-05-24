7a166ad7da86e0ccc1975ae69367aba559b236d3
Theo Sun <theo.sun@outlook.com>
2017-05-25 00:03:35 +0800

    update

--- routes/notification.js
+++ routes/notification.js
@@ -2,23 +2,32 @@ var express = require('express');
 var router = express.Router();
 var util = require("../lib/util");
 
-router.all("*", function (req, res, next) {
+router.all("*", async function (req, res, next) {
   var x_ns_token = req.get("x-ns-token");
   if (x_ns_token) {
-    req.locals.user = util.findUserByToken(x_ns_token);
-    next();
+    var user = await util.findUserByToken(x_ns_token);
+    if (user.length < 1) {
+      var err = new Error("No such token");
+      err.status = 401;
+      next(err);
+    } else {
+      var notification = await util.findNotificationByUser(user);
+      res.locals.user = user;
+      res.locals.notification = notification;
+      next();
+    }
   } else {
-    var err = new Error("need token first");
+    var err = new Error("Need token");
     err.status = 401;
     next(err);
   }
 });
 
-router.post(function (req, res, next) {
-
+router.post("*", function (req, res, next) {
+  
 });
 
-router.get(function (req, res, next) {
+router.get("*", function (req, res, next) {
 
 });
 
--- routes/user.js
+++ routes/user.js
@@ -2,16 +2,20 @@ var express = require('express');
 var router = express.Router();
 var util = require("../lib/util");
 
-router.all("/token", function (req, res, next) {
-  var user = req.body;
-  if (user && user.mail) {
-    util.saveNewToken(user);
-    res.json({
-      "status": "200",
-      "message": "check your email to get token"
-    });
-  } else {
-    next(new Error("Registe need a mail address"));
+router.all("/token", async function (req, res, next) {
+  try {
+    var user = req.body;
+    if (user && user.mail) {
+      await util.saveNewToken(user);
+      res.json({
+        "status": "200",
+        "message": "check your email to get token"
+      });
+    } else {
+      next(new Error("Registe need a mail address"));
+    }
+  } catch (error) {
+    next(error)
   }
 });
 
